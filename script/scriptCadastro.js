8078aa167e9ed6107f56729e315e1d07a49e560e
frohlich-pedro <pedro_h_nf@outlook.com>
2025-05-06 09:19:54 -0300

    update - scriptCadastro melhor

--- script/scriptCadastro.js
+++ script/scriptCadastro.js
@@ -1,21 +1,21 @@
-function validarForm() {
-    var tamanho_nome = document.forms["formcadastro"].nome.value.length;
-    var email = document.forms["formcadastro"].email.value;
-    var senha = document.forms["formcadastro"].senha.value;
-    var confirmarSenha = document.forms["formcadastro"].confirmarsenha.value;
+const validarForm = () => {
+    try {
+        let tamanho_nome = document.forms["formcadastro"].nome.value.length;
+        if (!/^['A-Za-zÀ-ÿ\s']$/.test(tamanho_nome)) {
+            throw ("O nome deve ter entre 5 e 64 caracteres.");
+        }
 
-    if (!/^['A-Za-zÀ-ÿ\s']$/.test(tamanho_nome)) {
-        alert("O nome deve ter entre 5 e 64 caracteres.");
-        return false
-    }
+        let email = document.forms["formcadastro"].email.value;
+        if (email.length < 5 || email.length > 128 || email.indexOf('@') == -1 || email.indexOf('.') == -1) {
+            throw("O email deve ser preenchido corretamente.")
+        }
 
-    if (email.length < 5 || email.length > 128 || email.indexOf('@') == -1 || email.indexOf('.') == -1) {
-        alert("O email deve ser preenchido corretamente.")
-        return false;
-    }
-    
-    if (senha !== confirmarSenha) {
-        alert("A senha e a confirmação de senha devem ser a mesma.")
-        return false;
+        let senha = document.forms["formcadastro"].senha.value;
+        let confirmarSenha = document.forms["formcadastro"].confirmarsenha.value;
+        if (senha !== confirmarSenha) {
+            throw("A senha e a confirmação de senha devem ser a mesma.")
+        }
+    } catch (e) {
+        alert(`ERRO: ${e}`);
     }
 }
