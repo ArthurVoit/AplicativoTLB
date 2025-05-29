acbc363b4b94ce25dd453aacf13a1879c1aef960
França-gui <guilherme_j_franca@estudante.sesisenai.org.br>
2025-05-29 12:57:15 -0300

    fixed server file selection

--- script/map.js
+++ script/map.js
@@ -1,4 +1,7 @@
-import * as THREE from 'three';
+import * as THREE from '../node_modules/three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+function main(){
  const scene = new THREE.Scene();
  let camera =  new THREE.PerspectiveCamera(75,window.innerHeight/window.innerWidth,100)
 const canvas = document.querySelector('#map');
@@ -7,11 +10,11 @@ const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
     document.body.appendChild(renderer.domElement);
 
     const geometry = new THREE.BoxGeometry(1, 3, 5);
-    const material = new THREE.MeshBasicMaterial({ color: 0x0000ff});
+    const material = new THREE.MeshBasicMaterial({ color: 0x0020ff});
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 
-    camera.position.z= 5
+    camera.position.z= 2
     function animate() {
         requestAnimationFrame(animate);
         cube.rotation.x += 0.01;
@@ -19,3 +22,5 @@ const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
     }
                    
     animate();
+}
+main();
