15648a4486f692a62c5f924d350b9842e03b15f1
França-gui <guilherme_j_franca@estudante.sesisenai.org.br>
2025-05-29 12:01:00 -0300

    server for running three.js files

--- script/map.js
+++ script/map.js
@@ -1,20 +1,17 @@
 import * as THREE from 'three';
  const scene = new THREE.Scene();
-
-    let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(100, 0, 0);
-
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth/2, window.innerHeight/2);
+ let camera =  new THREE.PerspectiveCamera(75,window.innerHeight/window.innerWidth,100)
+const canvas = document.querySelector('#map');
+const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
+    renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
     const geometry = new THREE.BoxGeometry(1, 3, 5);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const material = new THREE.MeshBasicMaterial({ color: 0x0000ff});
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 
-    controls.dragToLook = true;
-
+    camera.position.z= 5
     function animate() {
         requestAnimationFrame(animate);
         cube.rotation.x += 0.01;
