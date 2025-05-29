86f0d81a8e061d541bd88f40e467543dd33e53af
frohlich-pedro <pedro_h_nf@outlook.com>
2025-05-29 12:02:29 -0300

    update - teste pra corrigir server.mjs

--- script/server.mjs
+++ script/server.mjs
@@ -1,12 +1,13 @@
-// server.mjs
 import { createServer } from 'node:http';
+import { readFileSync } from 'node:fs';
+
+const html = readFileSync('../public/mapa.html', 'utf8');
+
 const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' })
-  ;
-  res.end('Hello World!\n');
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(html);
 });
-// starts a simple http server locally on port 3000
+
 server.listen(3000, '127.0.0.1', () => {
   console.log('Listening on 127.0.0.1:3000');
 });
-// run with `node server.mjs`
