cfaa9bceb2a83e2e35d10857ccdcb37406f99a55
Your Name <guilherme_j_franca@estudante.sesisenai.org.br>
2025-06-10 21:00:43 -0300

    map.js - Remoção do orbitcontrols por causa de relatórios do beta testing, separação das malhas para interatibilidade, adicionado raycasting para interatibilada

--- script/map.js
+++ script/map.js
@@ -1,17 +1,23 @@
+//import area
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.177.0/build/three.module.min.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.177.0/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.177.0/examples/jsm/controls/OrbitControls.js';
+import { Raycaster } from 'three';
 
 const scene = new THREE.Scene();
 
+
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
-camera.position.set(10, 10, 10);
+camera.position.set(-0, 0, 14);
 
 const canvas = document.querySelector('#map');
 const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+
+//loaded mesh
+
+//main area
 const loader = new GLTFLoader();
 loader.load('../assets/3D modules/trilhos -D.gltf', (gltf) => {
     gltf.scene.scale.set(102, 102, 102);
@@ -20,22 +26,54 @@ loader.load('../assets/3D modules/trilhos -D.gltf', (gltf) => {
     console.error('Error loading GLTF model:', error);
 });
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.dampingFactor = 0.05;
-controls.screenSpacePanning = false; 
-controls.minDistance = 1;
-controls.maxDistance = 50;
-controls.target.set(0, 1, 0); 
-controls.update();
+//Turn1 button
+loader.load('../assets/3D modules/T1B.gltf', (gltf) => {
+    gltf.scene.scale.set(102, 102, 102);
+    scene.add(gltf.scene);
+}, undefined, (error) => {
+    console.error('Error loading GLTF model:', error);
+});
 
+
+//Turn2 button
+loader.load('../assets/3D modules/T2B.gltf', (gltf) => {
+    gltf.scene.scale.set(102, 102, 102);
+    scene.add(gltf.scene);
+}, undefined, (error) => {
+    console.error('Error loading GLTF model:', error);
+});
+
+//Switch Sides button
+loader.load('../assets/3D modules/SSB.gltf', (gltf) => {
+    gltf.scene.scale.set(102, 102, 102);
+    scene.add(gltf.scene);
+}, undefined, (error) => {
+    console.error('Error loading GLTF model:', error);
+});
 
 const light = new THREE.AmbientLight(0xffffff, 1);
+
+//interectability
+const raycaster = new THREE.Raycaster(); 
+
+document.addEventListener("mousedown", onmousedown);
+
+function onmousedown(event){
+    const coords = new THREE.Vector2(
+        (event.clientX / renderer.domElement.clientWidth) * 2 
+        (event.clientY / renderer.domElement.clientHeight) * 2 
+    );
+    raycaster.setFromCamera(coords, camera);
+    const intersections = raycaster.intersectObjects(scene.children, true);
+    if (intersections.length > 0){
+        console.log(intersections)
+    }
+};
+
 scene.add(light);
 
 function animate() {
     requestAnimationFrame(animate);
-    controls.update();
     renderer.render(scene, camera);
 }
 
