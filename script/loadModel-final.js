03791df3e8456990b3f5ebd7805fd3ce7f962d22
andre-axt <andreaxtbachtold@gmail.com>
2025-06-12 22:53:48 -0300

    edit, add - frustrações envolvendo o three.js, primeira tentativa de colocar um trem 3D na página de monitoramento e manutenções.

---
+++ script/loadModel-final.js
@@ -0,0 +1,47 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/loaders/GLTFLoader.js';
+console.log('Script rodando');
+
+
+
+const cena = new THREE.Scene();
+
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderizador = new THREE.WebGLRenderer({ antialias: true });
+renderizador.setSize(window.innerWidth, window.innerHeight);
+document.getElementById('trem3D').appendChild(renderizador.domElement);
+
+
+const luz = new THREE.DirectionalLight(0xffffff, 1);
+luz.position.set(1, 1, 1).normalize();
+cena.add(luz);
+
+
+const loader = new GLTFLoader();
+console.log('Tentando carregar modelo: ../assets/trainv1.gltf');
+loader.load('../assets/3Dmodules/trainv1.gltf',
+  function (gltf) {
+    cena.add(gltf.scene);
+  },
+  undefined,
+  function (erro) {
+    console.error('Erro ao carregar o modelo:', erro);
+  }
+);
+
+
+camera.position.z = 5;
+
+
+function animar() {
+  requestAnimationFrame(animar);
+  renderizador.render(cena, camera);
+}
+animar();
