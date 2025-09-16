9a0cbbaea1aa106045ccb41dc0faf6a6fe54742a
andre-axt <andreaxtbachtold@gmail.com>
2025-09-16 12:36:17 -0300

    edit

--- public/cadastro.php
+++ public/cadastro.php
@@ -1,3 +1,26 @@
+<?php
+
+#include ../db.php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $nome = $_POST['nome_usuario'];
+    $email = $_POST['email_usuario'];
+    $senha = $_POST['senha_usuario'];
+    $confirmar_senha = $_POST['confirmar_senha'];
+
+    $sql = " INSERT INTO usuario (nome_usuario, email_usuario, senha_usuario) VALUEs ('$nome', '$email', '$senha')";
+
+    if ($conn->query($sql) === true) {
+        echo "Novo registro criado com sucesso.";
+    } else {
+        echo "Erro " . $sql . '<br>' . $conn->error;
+    }
+    $conn->close();
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,38 +36,38 @@
 <body class="bodyTelaCadastro">
     <section class="cadastro">
         <div class="formCadastro">
-            <a href="Login.html"><i class="bi bi-arrow-left-circle"></i></a>
+            <a href="login.php"><i class="bi bi-arrow-left-circle"></i></a>
             <form action="" id="formcadastro">
                 <br>
                 <br>
                 <div class="inputContainer">
-                    <input type="text" id="nome" name="nome" required placeholder="usuário">
+                    <input type="text" id="nome_usuario" name="nome_usuario" required placeholder="usuário">
                 <i class="bi bi-person-fill"></i>
                 </div>
                 <br>
                 <div class="inputContainer">
-                    <label for="email"></label>
-                    <input type="text" id="email" name="email" required placeholder="email">
+                    <label for="email_usuario"></label>
+                    <input type="email" id="email_usuario" name="email_usuario" required placeholder="email">
                     <i class="bi bi-at"></i>
                 </div>
                 <br>
                 <div class="inputContainer">
-                    <label for="senha"></label>
-                    <input type="password" id="senha" name="senha" required placeholder="senha">
+                    <label for="senha_usuario"></label>
+                    <input type="password" id="senha_usuario" name="senha_usuario" required placeholder="senha">
                     <i class="bi bi-lock-fill"></i>
                 </div>
                 <br>
                 <div class="inputContainer">
-                    <label for="confirmarsenha"></label>
-                    <input type="password" id="confirmarsenha" name="confirmarsenha" required placeholder="confirmar senha">
+                    <label for="confirmar_senha"></label>
+                    <input type="password" id="confirmar_senha" name="confirmar_senha" required placeholder="confirmar senha">
                     <i class="bi bi-lock-fill"></i>
                 </div>
                 <br>
                 <br>
-                <button class="buttonRoxoCadastro" type="button" onclick="validarForm()">Cadastrar-se</button>
+                <button class="buttonRoxoCadastro" type="submit">Cadastrar-se</button>
             </form>
             <p class="pCadastro">Ao clicar você estará de acordo com as políticas da empresa</p>
-            <a href="politicas.html">Ver políticas</a>
+            <a href="politicas.php">Ver políticas</a>
         </div>
     </section>
 </body>
--- script/scriptCadastro.js
+++ script/scriptCadastro.js
@@ -1,22 +1,22 @@
 const validarForm = () => {
     try {
-        let nome = document.forms["formcadastro"].nome.value;
-        if (nome.length < 5 || nome.length > 64) {
+        let nome_usuario = document.forms["formcadastro"].nome_usuario.value;
+        if (nome_usuario.length < 5 || nome_usuario.length > 64) {
             throw "O nome deve ter entre 5 e 64 caracteres.";
         }
 
-        let email = document.forms["formcadastro"].email.value;
+        let email_usuario = document.forms["formcadastro"].email_usuario.value;
         let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!emailRegex.test(email)) {
+        if (!emailRegex.test(email_usuario)) {
             throw "O email deve ser preenchido corretamente.";
         }
 
-        let senha = document.forms["formcadastro"].senha.value;
-        let confirmarSenha = document.forms["formcadastro"].confirmarsenha.value;
-        if (senha.length < 6) {
+        let senha_usuario = document.forms["formcadastro"].senha_usuario.value;
+        let confirmar_senha = document.forms["formcadastro"].confirmar_senha.value;
+        if (senha_usuario.length < 6) {
             throw "A senha deve ter pelo menos 6 caracteres.";
         }
-        if (senha !== confirmarSenha) {
+        if (senha_usuario !== confirmar_senha) {
             throw "A senha e a confirmação de senha devem ser iguais.";
         }
 
