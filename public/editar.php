36f606be873b8e9000595c055f871aaabdef20c0
Gui-Franca98 <guilhermejorgefranca2@gmail.com>
2025-09-25 12:56:18 -0300

    adição de pagina de edição

---
+++ public/editar.php
@@ -0,0 +1,32 @@
+<?php
+include '../db.php';
+session_start();
+
+$user = $_SESSION['id_usuario'];
+$sql = "SELECT * FROM usuario WHERE id_usuario=$user";
+$result = $conn->query($sql);
+echo"
+<table border = '1'>
+<tr>
+<th>   ID   </th>
+<th>  nome  </th>
+<th>  senha </th>
+<th>telefone</th>
+<th>  email </th>
+<th> função </th>
+</tr>
+";
+while($row = $result->fetch_assoc()){
+    echo"
+    <tr>
+    <td>{$row['id_usuario']}</td>
+    <td>{$row['nome_usuario']}</td>
+    <td>{$row['senha_usuario']}</td>
+    <td>{$row['telefone_usuario']}</td>
+    <td>{$row['email_usuario']}</td>
+    <td>{$row['funcao_usuario']}</td>
+    </tr>
+    ";
+};
+?>
+
