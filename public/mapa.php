6782b4a1f55cb854e19a45e7d76d6e8dcebdcc1b
Arthur <avoitreder@gmail.com>
2025-11-17 11:06:35 -0300

    edit

--- public/mapa.php
+++ public/mapa.php
@@ -11,13 +11,40 @@
     include "header.php";
 ?>
 
-    
-    <section>
-        <br>
-        <img src="../assets/img/mapaSA.png" alt="" >
-    </section>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mapa</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="bodyMapa">
 
+<section class="mapaAlterado">
+    <br>
+    <img src="../assets/img/mapaSA.png" alt="Mapa" style="position: relative;">
+    
+    <!-- Botão S1 com posicionamento correto -->
+    <div class="tooltip-container" style="position: absolute; top: 30%; left: 45%;">
+        <button class="btn-mapa btn-s1">S1</button>
+        <span class="tooltip-text">
+            SENSOR DE DISTANCIA<br>
+            SENSOR DE UMIDADE<br>
+            SENSOR DE LUMINOSIDADE
+        </span>
+    </div>
+    
+    <!-- Adicione mais botões conforme necessário -->
+    <!--
+    <div class="tooltip-container" style="position: absolute; top: X%; left: Y%;">
+        <button class="btn-mapa">S2</button>
+        <span class="tooltip-text">
+            Informações do sensor S2
+        </span>
+    </div>
+    -->
+</section>
 
 </body>
-
 </html>
